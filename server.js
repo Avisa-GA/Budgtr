89acd858a09aad9a1ef254ec525f01bff2b27837
Avisa-SEI <aap571420@gmail.com>
2021-06-03 14:02:31 -0700

    add: budget data into table

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 
-
+// import express
 const express = require("express");
+const budget = require("./models/budget.js");
 const app = express();
 const port = 3000;
 
+// import data model
 const models = require('./models/budget.js')
 
 
-
+// start making and calling routes
 app.get('/budget', (req, res) => {
-    res.send('HEllo World!')
+   
+    res.render('index.ejs', {
+        allBudget: budget
+    })
 })
 
-
+// listening to port
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
   })
